aa1bf56c5e42b533f4ed976054242082cba1f8c0
yfjelley <yfjelley@gmail.com>
2023-03-29 19:24:30 +0800

    xxx

--- ddbid/settings.py
+++ ddbid/settings.py
@@ -84,14 +84,9 @@ WSGI_APPLICATION = 'ddbid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'NAME': 'cqt_db',
-        'USER': 'root',
-        'PASSWORD': 'toor',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 # Internationalization
